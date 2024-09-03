354e5c8e4d060a91d748dd1254663a06d33b9fdd
Anthony deplanque <anthony.deplanque3184@gmail.com>
2024-09-03 19:03:07 +0200

    api

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const cors = require('cors');
+
+const cardRoutes = require('./routes/cardRoutes');
+const errorHandler = require('./middlewares/errorHandler');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static('public'));
+app.use('/cards', cardRoutes)
+app.use(errorHandler);
+
+app.listen(port, ()=>console.log(`server running on port ${port}`))
