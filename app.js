7626cba29ecaa1bcc911b87d2d867f8f66ceb1bf
Anthony deplanque <anthony.deplanque3184@gmail.com>
2024-09-03 19:35:18 +0200

    try to fix image call

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+const path = require('path');
+
 const cors = require('cors');
 
 const cardRoutes = require('./routes/cardRoutes');
@@ -13,7 +15,13 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/images/:filename', (req, res) => {
+  const filePath = path.join(__dirname, 'public/images', req.params.filename);
+  res.sendFile(filePath);
+});
+
 app.use('/cards', cardRoutes)
 app.use(errorHandler);
 
